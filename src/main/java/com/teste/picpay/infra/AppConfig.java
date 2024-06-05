d0a543eeb8e1d6cbde1118b8593c92e66423667d
Lucas Henrique <lucas.hcleonardo@gmail.com>
2024-06-05 16:23:05 -0300

    feat: General config

---
+++ src/main/java/com/teste/picpay/infra/AppConfig.java
@@ -0,0 +1,15 @@
+package com.teste.picpay.infra;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+}
